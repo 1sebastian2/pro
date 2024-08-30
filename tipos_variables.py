032b56b5166ea9d450eed7c31e5fec02e9514028
Sebastian Ortiz <sebastianortizlara669@gmail.com>
2024-08-30 14:17:32 -0500

    programacion

---
+++ tipos_variables.py
@@ -0,0 +1,11 @@
+#tipos de variables
+cadena = "github"
+print(type(cadena))
+numero = 10
+print(type(numero))
+decimal = 1.190
+print(type(decimal))
+boollean = True
+print(type(boollean))
+letra = "a"
+print(type(letra))
