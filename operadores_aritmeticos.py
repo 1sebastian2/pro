780266b72ff759c0668d75e3fdf305d908a16b49
Sebastian Ortiz <sebastianortizlara669@gmail.com>
2024-08-30 19:20:36 -0500

    primer commit

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,19 @@
+#importadores_aritmeticos
+import math
+#ejemplo 1
+ejemplo1 = 5*(2+5)
+# imprimirvariables 
+print(f"el resultado de la operacion es: {ejemplo1}")
+#Ejemplo 2 
+ejemplo2 = (4+3)-((3*2)+1)
+# imprimirvariables
+print(f"el resultado de la operacion es: {ejemplo2}")
+#ejemplo 3
+ejemplo3 = 2*(3+(5*(9/3)))
+# imprimirvariable
+print(f"el resultado de la operacion es: {ejemplo3}")
+#ejemplo 4
+ejemplo4 = 7+3*(1+5*(2*(9-7)+4)+2)
+# imprimirvariables  
+print(f"el resultado de la operacion es: {ejemplo4}")
+
