97f29f676cd789468e4fcb8750953758bd52b11c
Sebastian Ortiz <sebastianortizlara669@gmail.com>
2024-09-13 16:57:01 -0500

    ejercicio

---
+++ ejemplo_while.py
@@ -0,0 +1,21 @@
+# estructura_repeticion
+opcion = int(input("digite la opcion del menu: "))
+resultado = 1+2
+
+while opcion != "salir":
+    if opcion == "1":
+        print(f"suma = (resultado)")
+    elif opcion == "2":
+        print("resta=", 3-4)
+    elif opcion == "3":
+        print("multiplicasion=", 3*4)
+    elif opcion == "4":
+        print("division=", 3/4)
+    elif opcion == "5":
+        print("poten=", 3**4)
+    
+
+
+
+
+
