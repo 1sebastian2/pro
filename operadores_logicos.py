a3a89ee0eaee5d537ca7750cd36491423532d01e
Sebastian Ortiz <sebastianortizlara669@gmail.com>
2024-09-13 15:30:23 -0500

    operadores

---
+++ operadores_logicos.py
@@ -0,0 +1,62 @@
+#variable
+num1 = 2
+num2 = 2
+#ol
+#AND
+print(num1 == num2 and num1 <= num2 ) # 1-1 v
+print(num1 == num2 and num1 > num2 ) # 1 0-f
+print(num1 < num2 and num1 < num2 ) # o o-f
+print(num1 > num2 and num1 >= num2 ) # 0 1-f
+
+
+#ejemplo 1
+#usuario = input("digite el usuario: ")
+#contraseña = input("digite la contraseña: ")
+#ADN
+#print(usuario == "sebastian12" and contraseña == "seb23!" )
+
+print(num1 == num2 or num1 <= num2 ) # 0 1 v
+print(num1 <= num2 or num1 > num2 ) # 1 0 - v
+print(num1 < num2 or num1 > num2 ) # 0 0 - f
+print(num1 > num2 or num1 <= num2 ) # 0 1 
+
+#NOT
+print(not False) #V
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
