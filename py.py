6f202359e86dc8fb2b55b703136976fd6cca751d
sebastian ortiz <sebastianortizlara669@gmail.com>
2024-10-18 15:24:08 -0500

    #

--- py.py
+++ py.py
@@ -1,14 +1,21 @@
+#Tablero de ajedrez
+# Solicitar el tamaño del tablero al usuario
+filas = int(input("Introduce el número de filas del tablero: "))
+columnas = int(input("Introduce el número de columnas del tablero: "))
 
-tablero = [['' for _ in range(8)] for _ in range(8)]
-
-
-for fila in range(6):
-    for columna in range(6):
-        if (fila + columna) % 2 == 0:
-            tablero[fila][columna] = '■'  
+# Crear el tablero de ajedrez
+tablero_ajedrez = []
+for i in range(filas):
+    fila = []
+    for j in range(columnas):
+        if (i + j) % 2 == 0:
+            fila.append('■')
         else:
-            tablero[fila][columna] = '□'  
+            fila.append("□")
+    tablero_ajedrez.append(fila)
 
-
-for fila in tablero:
-    print(' '.join(fila))
+# Imprimir el tablero de ajedrez
+for fila in tablero_ajedrez:
+    for casilla in fila:
+        print(casilla, end=" ")
+    print()  # Nueva línea al final de cada fila
